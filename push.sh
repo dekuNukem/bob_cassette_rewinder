0bc65fb5cf3e39d635d2d6c3d462c8fcf7397918
dekunukem <dekunukem@gmail.com>
2021-04-03 02:04:43 +0100

    clean up

--- push.sh
+++ push.sh
@@ -9,10 +9,6 @@ find . -type f -name "*.l#*" -exec rm -f {} \;
 find . -type f -name "*.csv#*" -exec rm -f {} \;
 find . -type f -name "*.DS_Store*" -exec rm -f {} \;
 find . -name "__pycache__" -exec rm -rf {} \;
-find ./pc_software -name "build" -exec rm -rf {} \;
-find ./pc_software -name "dist" -exec rm -rf {} \;
-find ./pc_software -name "*.zip" -exec rm -rf {} \;
-find ./pc_software -type f -name "*.spec*" -exec rm -f {} \;
 
 git add --all
 git commit -m "$@"
